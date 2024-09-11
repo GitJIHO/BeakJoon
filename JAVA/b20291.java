32736a0568c48231646ec39902ac0186e71267f1
gitjiho <jiho99322@gmail.com>
2024-09-11 19:05:23 +0900

    fix: b20291 파일 정리

--- JAVA/b20291.java
+++ JAVA/b20291.java
@@ -2,17 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class b20291{
+
+	static int txt;
+	static int num;
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
 		String extension[] = new String[n];
 		ArrayList<String> strArray = new ArrayList<String>();
-		int txt=0;
+		txt=0;
 		HashMap<String,Integer> hm = new HashMap<String,Integer>();
 		for(int i=0;i<n;i++) {
-			int num=0;
+			num = 0;
 			String str = br.readLine();
 			extension[i]=str.split("\\.")[1];
 			hm.put(extension[i], (hm.getOrDefault(extension[i], 0)+1));
