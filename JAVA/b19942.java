78e3d876ba52e9520a398cc0657c772ec92d7847
gitjiho <jiho99322@gmail.com>
2025-02-15 22:47:16 +0900

    solve: b19942 다이어트

--- JAVA/b19942.java
+++ JAVA/b19942.java
@@ -2,92 +2,85 @@ import java.util.*;
 import java.io.*;
 
 public class b19942 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
-    static int[][] arr;
-    static int result = Integer.MAX_VALUE;
-    static int[] min_nutrution;
-    static int[] isSelected;
-    static List<String> list;
+    static int arr[][]; 
+    static int result = Integer.MAX_VALUE; 
+    static int min_nutrution[]; 
+    static int isSelected[]; 
+    static ArrayList<String> list; 
 
-    static void input() throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1][5];
+
+        arr = new int[N+1][5];
         min_nutrution = new int[4];
         list = new ArrayList<>();
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
+
+        for(int i=0;i<4;i++){
             min_nutrution[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= N; i++) {
+        for(int i=1;i<=N;i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
+            for(int j=0;j<5;j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            isSelected = new int[i];
-            dfs(0, i, 1);
+        for(int i=1;i<=N;i++){
+            isSelected = new int[N];
+            dfs(0,i,1);
+        }
+
+        if(result == Integer.MAX_VALUE) System.out.println(-1);
+        else{
+            Collections.sort(list);
+            System.out.println(result);
+            System.out.println(list.get(0));
         }
     }
 
-    static void dfs(int cnt, int choiceNum, int start) {
-        if (cnt == choiceNum) {
-            checkNutrition(choiceNum);
+    static void dfs(int cnt,int choiceNum,int start){
+        if(cnt == choiceNum){
+            isCheck(choiceNum);
             return;
         }
 
-        for (int i = start; i <= N; i++) {
-            isSelected[cnt] = i;
-            dfs(cnt + 1, choiceNum, i + 1);
+        for(int i=start;i<=N;i++){
+            isSelected[cnt] = i; 
+            dfs(cnt+1,choiceNum,i+1); 
         }
     }
 
-    static void checkNutrition(int choiceNum) {
-        int[] sum_nutrition = new int[4];
-        int price = 0;
-
-        for (int i = 0; i < choiceNum; i++) {
-            for (int j = 0; j < 4; j++) {
+    static void isCheck(int choiceNum){
+    
+        int sum_nutrition[] = new int[4]; 
+        int price = 0; 
+        
+        for(int i=0;i<choiceNum;i++){
+            for(int j=0;j<4;j++){
                 sum_nutrition[j] += arr[isSelected[i]][j];
             }
             price += arr[isSelected[i]][4];
         }
 
-        for (int i = 0; i < 4; i++) {
-            if (sum_nutrition[i] < min_nutrution[i]) {
-                return;
-            }
-        }
-
-        if (result >= price) {
-            if (result > price) {
-                list.clear();
-            }
+        for(int i=0;i<4;i++)
+            if(min_nutrution[i] > sum_nutrition[i]) return ;
+            
+        if(result >= price){
+            if(result > price) list.clear();
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < choiceNum; i++) {
-                sb.append(isSelected[i]).append(" ");
+            for(int i=0;i<choiceNum;i++){
+                sb.append(isSelected[i]+" ");
             }
-            list.add(sb.toString().trim());
+
+            list.add(sb.toString());
             result = price;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        input();
-        bw.write(String.valueOf(result));
-        if (result != Integer.MAX_VALUE) {
-            Collections.sort(list);
-            bw.newLine();
-            bw.write(list.get(0));
-        }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
