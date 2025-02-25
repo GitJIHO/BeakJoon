733051050b369a39ca216be0992c49647b7691bb
gitjiho <jiho99322@gmail.com>
2025-02-25 23:10:32 +0900

    solve: b12865 평범한 배낭

--- JAVA/b12865.java
+++ JAVA/b12865.java
@@ -5,31 +5,27 @@ public class b12865 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N, K;
-    static int[][] dp;
+    static int[] dp;
 
     static void input() throws IOException {
         String[] input = br.readLine().split(" ");
         N = Integer.parseInt(input[0]);
         K = Integer.parseInt(input[1]);
 
-        dp = new int[N+1][K+1];
+        dp = new int[K+1];
 
         for (int i=1; i<=N; i++) {
             input = br.readLine().split(" ");
             int weight = Integer.parseInt(input[0]);
             int value = Integer.parseInt(input[1]);
-            for (int j=1; j<=K; j++) {
-                if (j >= weight) {
-                    dp[i][j] = Math.max(dp[i-1][j-weight]+value, dp[i-1][j]);
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
+            for (int j=K; j>=weight; j--) {
+                dp[j] = Math.max(dp[j-weight]+value, dp[j]);
             }
         }
     }
 
     static void result() throws IOException {
-        bw.write(String.valueOf(dp[N][K]));
+        bw.write(String.valueOf(dp[K]));
     }
 
     public static void main(String[] args) throws IOException {
