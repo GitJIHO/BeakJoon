c31da8e0eeb89e9e7c3e7e6aa121321b3ed87213
gitjiho <jiho99322@gmail.com>
2025-06-24 14:22:10 +0900

    solve: b1850 최대공약수

---
+++ JAVA/b1850.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ 
+public class b1850 {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+        
+        long gcd = getGCD(Math.max(num1, num2), Math.min(num1, num2));
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i <= gcd; i++) sb.append("1");
+        
+        System.out.println(sb.toString());
+            
+    }
+    
+    public static long getGCD(long a, long b) {
+        while(b > 0) {
+            long tmp = a;
+            a = b;
+            b = tmp%b;
+        }
+        return a;
+    }
+}
