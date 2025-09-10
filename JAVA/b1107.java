07abc3432e583d72a8965f0df08a0788fce66981
gitjiho <jiho99322@gmail.com>
2025-09-10 13:19:46 +0900

    solve: b1107 리모컨cd

---
+++ JAVA/b1107.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class b1107 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int target, N, count;
+    static int[] dead;
+    
+    static void input() throws IOException {
+        target = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        dead = new int[N];
+        if (N > 0) { // 고장난 버튼이 있을 때만 입력 받기
+            String[] input = br.readLine().split(" ");
+            for (int i=0; i<N; i++) {
+                dead[i] = Integer.parseInt(input[i]);
+            }
+        }
+    }
+
+static void cal() {
+    count = Math.abs(target - 100);
+
+    for (int ch = 0; ch <= 999999; ch++) {
+        if (canPress(ch)) {
+            int pressCount = String.valueOf(ch).length();
+            pressCount += Math.abs(target - ch);
+            count = Math.min(count, pressCount);
+        }
+    }
+}
+
+static boolean canPress(int ch) {
+    if (ch == 0) {
+        for (int d : dead) {
+            if (d == 0) return false;
+        }
+        return true;
+    }
+
+    while (ch > 0) {
+        int digit = ch % 10;
+        for (int d : dead) {
+            if (digit == d) return false;
+        }
+        ch /= 10;
+    }
+    return true;
+}
+
+    public static void main(String[] args) throws IOException {
+        input();
+        cal();
+        bw.write(String.valueOf(count));
+        bw.close(); br.close();
+    }
+}
